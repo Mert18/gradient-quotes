086d29149b5585fa555cb0e8aaf5b248d5af51ab
mert18 <mertuygur02@gmail.com>
2021-09-30 21:38:18 +0300

    hehe

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React, { useRef } from "react";
 import colors from "./data/colors.json";
 import quotes from "./data/quotes.json";
 import "./App.css";
+import html2canvas from "html2canvas";
 
 const App = () => {
-  const componentRef = useRef<HTMLDivElement>(null);
-
   const [quote, setQuote] = React.useState("");
   const [author, setAuthor] = React.useState("");
-
+  const printRef = useRef<HTMLDivElement>(null);
   const [deg, setDeg] = React.useState(0);
   const [col1, setCol1] = React.useState("B4A6AB");
   const [col2, setCol2] = React.useState("615055");
@@ -27,13 +26,27 @@ const App = () => {
     setAuthor(quotes[randomNumber].author);
   };
 
+  const handleDownload = async () => {
+    const element: any = printRef.current;
+    const canvas = await html2canvas(element);
+
+    const data = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+
+    if (typeof link.download === "string") {
+      link.href = data;
+      link.download = "image.png";
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
+    }
+  };
+
   return (
-    <div
-      className="container"
-      style={{
-        backgroundImage: `linear-gradient(${deg}deg,#${col1}, #${col2} )`,
-      }}
-    >
+    <div className="container">
       <div className="header">
         <div className="formwrapper">
           <form className="form">
@@ -75,7 +88,7 @@ const App = () => {
       </div>
       <div className="main">
         <div
-          ref={componentRef}
+          ref={printRef}
           className="gradcomponent"
           style={{
             backgroundImage: `linear-gradient(${deg}deg,#${col1}, #${col2} )`,
@@ -89,6 +102,9 @@ const App = () => {
               <p>{author}</p>
             </div>
           </div>
+        </div>
+        <div className="downloads">
+          <button onClick={handleDownload}>Download</button>
         </div>
       </div>
     </div>
